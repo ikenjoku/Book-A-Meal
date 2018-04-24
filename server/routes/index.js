823cdf8cfdb5c4edbd132013214c5e004f3a9146
Ike Njoku <ikeenjoku@gmail.com>
2018-04-24 11:55:10 +0100

    Update routes with /menu routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,4 +1,5 @@
 const mealController = require('../controllers').meals;
+const menuController = require('..controllers').menu;
 
 module.exports = (app) => {
     app.get('/api/v1', (req, res) => res.status(200).send({
@@ -10,4 +11,7 @@ module.exports = (app) => {
 
     app.put('/api/v1/meals/:id', mealController.update);
     app.delete('/api/v1/meals/:id', mealController.destroy);
+
+    app.get('/api/v1/menu', menuController.list);
+    app.post('/api/v1/menu', menuController.create);
 };
