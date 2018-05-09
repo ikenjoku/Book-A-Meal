2d3820e4778dfc3cde6c8c24d2f82e4b4218693f
Ike Njoku <ikeenjoku@gmail.com>
2018-05-09 08:44:21 +0100

    fix(implement feedback): add db url

--- server/config/config.js
+++ server/config/config.js
@@ -11,10 +11,9 @@ module.exports = {
     dialect: process.env.dialect,
   },
   test: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'postgres://zveddkuy:2L2j7IktvjDteqZEO0_kr_YfK2h89C8W@elmer.db.elephantsql.com:5432/zveddkuy',
   },
   production: {
-    use_env_variable: 'DATABASE_URL_TEST',
+    use_env_variable: 'postgres://nqoyugta:vhfjgx6Yi-cK62F6m3n4vNR7wREhkNgw@elmer.db.elephantsql.com:5432/nqoyugta',
   },
 };
-
