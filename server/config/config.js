f3d114d5c9433bebdc2c5b503da7b34359aef509
Ike Njoku <ikeenjoku@gmail.com>
2018-05-09 06:36:14 +0100

    fix(implement feedback): use enviromental variables in config file

---
+++ server/config/config.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.username,
+    password: process.env.password,
+    database: process.env.database,
+    host: process.env.host,
+    dialect: process.env.dialect,
+  },
+  test: {
+    use_env_variable: 'DATABASE_URL',
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL_TEST',
+  },
+};
+
