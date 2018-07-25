455dcf6745e67648aa10446303603845f6a940da
Ike Njoku <ikeenjoku@gmail.com>
2018-07-25 11:37:11 +0100

    fix(heroku deploy): update api url

--- client/axiosConfig.js
+++ client/axiosConfig.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+let api = '/api/v1';
+
+api = process.env.NODE_ENV === ('development' || 'test') ?
+  `http://localhost:9000${api}` : api;
+
 export default axios.create({
-  baseURL: 'http://localhost:9000/api/v1',
+  baseURL: api,
 });
